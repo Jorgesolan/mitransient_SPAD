aa2786f43d61a71165ecd862c38433e96c385033
Jorge Solan Morote <816259@unizar.es>
2024-09-23 20:01:54 +0200

    Implementacion modelo Computacional de un SPAD en Simulación | Crosstalk en y-tal pendiente

--- mitransient/films/transient_hdr_film.py
+++ mitransient/films/transient_hdr_film.py
@@ -1,8 +1,46 @@
 import mitsuba as mi
 import drjit as dr
 
+from mitransient.utils import speed_of_light
 from mitsuba import is_monochromatic, is_spectral
 from mitransient.render.transient_block import TransientBlock
+
+def read_tj(path, volt):
+    time = dr.zeros(mi.Float, 1480)
+    pdf = dr.zeros(mi.Float, 1480)
+
+    mx_pdf = 0
+    mx_pdf_index = 0
+
+    with open(path, 'r') as f:
+        f.readline()
+        for i in range(1480):
+            t, vsub20, vsub5 = f.readline().split(',')
+            time[i] = float(t)
+            pdf[i] = float(vsub20) if volt == 20 else float(vsub5) if volt == 5 else 0.0
+            
+            # Find the index of the maximum value in pdf
+            if pdf[i] > mx_pdf:
+                mx_pdf = pdf[i]
+                mx_pdf_index = i
+    # Adjust the spad time
+    time = time - time[mx_pdf_index]
+    time = mi.Float(time)
+    return time, pdf
+
+
+def add_transient_spad(self, sampler, active):
+    if self.use_spad:
+        sample = sampler.next_2d()
+        mask_spad = sample[1] < self.spad_lost # The SPAD detects a % of the photons that reach it
+        index, _ = self.disc.sample_reuse(sample[0], active)
+        
+        result = dr.gather(dtype=type(self.time), source=self.time, index=index) # Sample the arrival time
+        dist_spad = result * speed_of_light * self.mod_spad # Convert time to distance
+    else:
+        mask_spad = mi.Mask(True)
+        dist_spad = mi.Float(0.0)
+    return dist_spad, mask_spad
 
 
 class TransientHDRFilm(mi.Film):
@@ -53,15 +91,22 @@ class TransientHDRFilm(mi.Film):
         self.bin_width_opl = props.get("bin_width_opl", mi.Float(0.003))
         self.start_opl = props.get("start_opl", mi.UInt32(0))
 
+        self.use_spad = props.get("use_spad", False) # Boolean to enable SPAD
+        self.spad_volt = props.get("spad_volt", 5) # Voltage of the SPAD
+        self.spad_lost = props.get("spad_lost", 1) # Percentage of lost photons
+        self.path = props.get("spad_csv", "../../SPAD/20um_cmos.csv") # Path to the SPAD CSV file
+        self.mod_spad = props.get("mod_spad", 1) # Float to modify the SPAD time-jitter
+
         dr.make_opaque(self.temporal_bins, self.bin_width_opl, self.start_opl)
 
     def end_opl(self):
         return self.start_opl + self.bin_width_opl * self.temporal_bins
 
-    def add_transient_data(self, spec, distance, wavelengths, active, pos, ray_weight):
+    def add_transient_data(self, spec, sampler ,distance, wavelengths, active, pos, ray_weight):
         """
         Add a path's contribution to the film
         * spec: Spectrum / contribution of the path
+        * sampler: sampler
         * extra_weight: WIP. Hidden Geometry Rejection Sampling stuff.
         * distance: distance traveled by the path (opl)
         * wavelengths: for spectral rendering, wavelengths sampled
@@ -69,9 +114,13 @@ class TransientHDRFilm(mi.Film):
         * pos: pixel position
         * ray_weight: weight of the ray given by the sensor
         """
-        idd = (distance - self.start_opl) / self.bin_width_opl
+
+        dist_spad, mask_spad = add_transient_spad(self, sampler, active)
+        # Obtains the values of the capture efficiency and the time-jitter.
+
+        idd = (distance + dist_spad - self.start_opl) / self.bin_width_opl
         coords = mi.Vector3f(pos.x, pos.y, idd)
-        mask = (idd >= 0) & (idd < self.temporal_bins)
+        mask = (idd >= 0) & (idd < self.temporal_bins) & mask_spad
         self.transient.put(
             pos=coords,
             wavelengths=wavelengths,
